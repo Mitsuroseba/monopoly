982265a2a798f775aacee3e15b5e23fab594b037
Ruslan Danilin <rdanilin@gmail.com>
2011-11-16 09:41:24 +0200

    base eclipse project with libs.

---
+++ src/com/bostonunisoft/students/monopoly/Runner.java
@@ -0,0 +1,12 @@
+package com.bostonunisoft.students.monopoly;
+
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("Hello Monopoly!");
+	}
+
+}
